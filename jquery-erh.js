ab9a7944f0e51e3a6e1cfb41e5bf1f05bfee2a88
Rikard <rikard@erh.se>
2017-12-10 20:34:02 +0100

    Fixed some issues

--- jquery-erh.js
+++ jquery-erh.js
@@ -1,32 +1,36 @@
 /*
  * Parses datestring and return date with added days
  */
-$.fn.calcdate = function(datestring, add) {
-	d = new Date(Date.parse(datestring));
-	var day = d.getDate();
-	d.setDate(day+add);
-	
-	var day = d.getDate();
-	var month = d.getMonth() + 1;
-	var year = d.getFullYear();
-	if (day < 10) {
-		day = "0" + day;
-	}
-	if (month < 10) {
-		month = "0" + month;
-	}
-	var date = year + "-" + month + "-" + day;
-	
-	return date;
+
+$.date = function(dateObject) {
+    var d = new Date(dateObject);
+    var day = d.getDate();
+    var month = d.getMonth() + 1;
+    var year = d.getFullYear();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    var date = year + "-" + month + "-" + day;
+
+    return date;
 };
-	
+
+$.calcdate = function(datestring) {
+    d = new Date(Date.parse(datestring));
+    var day = d.getDate();
+    d.setDate(day+30);
+    return $.date(d);
+};
 
 /*
  * Gives true if element exist
  */
 $.fn.exists = function() {
-	return this.length > 0;
-}
+    return this.length > 0;
+};
 
 
 /**
